53a6d803b38dcb7db5111434e8aed3afe74019f2
Fabiana <39682297+fabianamasini@users.noreply.github.com>
2020-12-08 17:34:38 -0300

    MongoDB database and collection (#6)
    * Mongo db connection init
    
    * reformatting files
    
    * added preprocessed data in mongo db

--- classification/preprocessing.py
+++ classification/preprocessing.py
@@ -1,37 +1,39 @@
-import pandas as pd 
+import pandas as pd
 import nltk
 import re
 import spacy
 
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
+from data.mongo import Mongo
 
 
 class Preprocessing:
 
     def __init__(self):
-        self.fake_news_dataset = pd.read_csv("data/Fake.csv")
-        self.true_news_dataset = pd.read_csv("data/True.csv")
+        self.mongo = Mongo()
+        self.fake_news_dataset = self.mongo.read_mongo("fake_news")
+        self.real_news_dataset = self.mongo.read_mongo("real_news")
         self.stops = stopwords.words('english')
         self.spc = spacy.load('en_core_web_sm')
 
         nltk.download('punkt')
         nltk.download('stopwords')
-    
+
     def csv_head(self, dataset):
-        """ 
-        Prints the first 5 rows of each dataset 
+        """
+        Prints the first 5 rows of each dataset
         """
         print(dataset.head())
 
     def tokenize(self, text):
-        """ 
-        Tokenizes the text 
+        """
+        Tokenizes the text
         """
         return word_tokenize(text)
 
     def only_lowercased_letters(self, text):
-        """ 
+        """
         Removes all punctuation from the text and
         lowercases all words and letters
         """
@@ -50,19 +52,21 @@ class Preprocessing:
         Lemmatizes important words
         """
         spc_words = self.spc(important_words)
-        lemmas = [tokens.lemma_ if tokens.pos_ == 'VERB' else str(tokens) for tokens in spc_words]
+        lemmas = [
+            tokens.lemma_ if tokens.pos_ == 'VERB'
+            else str(tokens) for tokens in spc_words
+            ]
+
         clean_text = " ".join(lemmas)
         return clean_text
 
     def clean_text(self, text):
-        print("Tokeninzing . . .")
         token = self.tokenize(text)
 
-        print("Removing stop words . . .")
-        important_words = self.remove_stopwords(self.only_lowercased_letters(text))
+        important_words = self.remove_stopwords(
+            self.only_lowercased_letters(text)
+            )
 
-        print("Lemmas . . .")
         lemmas = self.lemmatize(important_words, token)
 
         return lemmas
-
---
+++ data/mongo.py
@@ -0,0 +1,27 @@
+from pymongo import MongoClient
+import pandas as pd
+
+
+class Mongo:
+
+    def __init__(self):
+        self.uri = "mongodb://localhost:27017/"
+
+        self.client = MongoClient(self.uri)
+        self.db = self.client.corgi4news
+
+    def read_mongo(self, collection, no_id=True):
+        """
+        Reads all data from a Mongo collection
+        and stores into DataFrame
+        """
+        cursor = self.db[collection].find()
+        df = pd.DataFrame(list(cursor))
+        if no_id:
+            del df['_id']
+        return df
+
+    def insert_into_collection(self, title, collection, column, data):
+        my_query = {"title": title}
+        new_value = {"$set": {column: data}}
+        self.db[collection].update(my_query, new_value)
