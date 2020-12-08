37faa438ecda90c35dd269f376e9bf1470c9337d
Fabiana Masini Garcia <masinifabiana@gmail.com>
2020-12-08 17:42:41 -0300

    feature extraction class

---
+++ classification/feature_extraction.py
@@ -0,0 +1,16 @@
+class FeatureExtraction:
+
+    def __init__(self):
+        return
+
+    def bag_of_words(self):
+        return
+
+    def wordclouds(self):
+        return
+
+    def tf_idf(self):
+        return
+
+    def ngrams(self):
+        return
